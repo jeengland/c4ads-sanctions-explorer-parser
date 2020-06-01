1054ef7e2f9339213f0ee3b9f737de0af7464b67
unknown <jacob.e.england@gmail.com>
2020-06-01 16:38:33 -0400

    Added logic to remove null values

--- parser.js
+++ parser.js
@@ -4,13 +4,16 @@ const fs = require('fs')
 csv()
     .fromFile('./data/SDN-00-04-joined.csv')
     .then((results) => {
-        let newResults = {}
-        for (const prop in results {
-            if (results[prop]) {
-                newResults[prop] = results[prop]
+        const newResults = results.map((result) => {
+            let newResult = {}
+            for (const prop in result) {
+                if (result[prop]) {
+                    newResult[prop] = result[prop]
+                }
             }
-        }
-        return newResults
+            return newResult
+            })
+        return newResults;
     })
     .then((results) => {
         fs.writeFile('./json/SDN-00-04-joined.json', JSON.stringify(results, null, "\t"), function (err) {
