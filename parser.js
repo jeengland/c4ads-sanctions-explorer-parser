09e68a792321b3e0bf3e225b94484fa30caeaa84
unknown <jacob.e.england@gmail.com>
2020-06-01 16:58:17 -0400

    Added function for getting all file names in a directory

--- parser.js
+++ parser.js
@@ -1,23 +1,42 @@
 const csv = require('csvtojson')
 const fs = require('fs')
 
-csv()
-    .fromFile('./data/SDN-00-04-joined.csv')
-    .then((results) => {
-        const newResults = results.map((result) => {
-            let newResult = {}
-            for (const prop in result) {
-                if (result[prop]) {
-                    newResult[prop] = result[prop]
+const parser = (filename, dir) => {
+    csv()
+        .fromFile(`${dir}/${filename}.csv`)
+        .then((results) => {
+            const newResults = results.map((result) => {
+                let newResult = {}
+                for (const prop in result) {
+                    if (result[prop]) {
+                        newResult[prop] = result[prop]
+                    }
                 }
-            }
-            return newResult
-            })
-        return newResults;
-    })
-    .then((results) => {
-        fs.writeFile('./json/SDN-00-04-joined.json', JSON.stringify(results, null, "\t"), function (err) {
-            if (err) throw err;
-            console.log('Created file successfully');
+                return newResult
+                })
+            return newResults;
         })
+        .then((results) => {
+            fs.writeFile(`./json/${filename}.json`, JSON.stringify(results, null, "\t"), (err) => {
+                if (err) {
+                    throw err
+                };
+                console.log('Created file successfully');
+            })
+        })
+}
+
+const getFileNames = (dir) => {
+    fs.readdir(dir, (err, files) => {
+        if (err) {
+            throw err;
+        }
+        const formattedFiles = files.map((file) => {
+            return file.slice(0, file.indexOf('.csv'))
+        })
+        console.log(formattedFiles);
+        return formattedFiles;
     })
+}
+
+getFileNames('./data')
