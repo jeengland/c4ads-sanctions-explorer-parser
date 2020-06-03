073cf39cf3b33514d0eb2fc69f66a215534f6747
unknown <jacob.e.england@gmail.com>
2020-06-03 18:56:19 -0400

    Added other entity types to formatter.js

--- formatter.js
+++ formatter.js
@@ -71,59 +71,140 @@ const formatter = (json) => {
     return json;
 }
 
-const tableKeys = filter.individual_keys;
-
 parser.parse('ofac', './data', formatter)
     .then((results) => {
-        startTime = Date.now();
-        let ind = {};
-        ind.individual = [];
-        ind.individual_attributes = [];
-        ind.individual_contact_info = [];
-        ind.individual_actions = [];
-        ind.individual_remarks = [];
-        let idList = {};
-        results.forEach((result) => {
-            if (result.entity_type === 'individual') {
-                let id = null;
-                if (result.first_name && result.last_name) {
-                    id = `${result.first_name}${result.last_name}`
-                } else if (result.first_name) {
-                    id = result.first_name
-                } else if (result.last_name) {
-                    id = result.last_name
-                } else if (result.entity_name) {
-                    id = result.entity_name
-                }
-                let hash = crypto.createHash('sha1');
-                result.individual_id = hash.update(id, 'utf8').digest('base64')
-                for (const table in ind) {
-                    if (table === 'individual' && idList[result.individual_id]) {
-                        // I can't get this statement to work inversely for some reason
-                    } else {
-                        const newTable = {}
-                        tableKeys[table].forEach((key) => {
-                            if (result[key]) {
-                                newTable[key] = result[key]
-                            }
-                        })
-                        if (Object.keys(newTable).length > 1) {
-                            ind[table].push(newTable)
-                        }
-                        if (table === 'individual') {
-                            idList[result.individual_id] = true;
-                        }
-                    }    
-                }
+        for (tableKeys in filter) {
+            let entityTable = {};
+            for (const table in filter[tableKeys]) {
+                entityTable[table] = [];
             }
-        })
-        // console.log(`${ind.individual.length} unique individuals`)
-        // console.log(`${ind.individual_attributes.length}`)
-        fs.writeFile(`./json/individuals.json`, JSON.stringify(ind, null, "\t"), (err) => {
-            if (err) {
-                throw err;
-            };
-            totalTime = Date.now() - startTime;
-            console.log(`Created individuals.json successfully in ${totalTime} ms`);
-        })
+            let idList = {};
+            results.forEach((result) => {
+                if (tableKeys === 'individual') {
+                    if (result.entity_type === 'individual') {
+                        let id = null;
+                        if (result.first_name && result.last_name) {
+                            id = `${result.first_name}${result.last_name}`
+                        } else if (result.first_name) {
+                            id = result.first_name
+                        } else if (result.last_name) {
+                            id = result.last_name
+                        } else if (result.entity_name) {
+                            id = result.entity_name
+                        }
+                        let hash = crypto.createHash('sha1');
+                        result.individual_id = hash.update(id, 'utf8').digest('base64')
+                        for (const table in entityTable) {
+                            if (table === 'individual' && idList[result.individual_id]) {
+                                // I can't get this statement to work inversely for some reason
+                            } else {
+                                const newTable = {}
+                                filter[tableKeys][table].forEach((key) => {
+                                    if (result[key]) {
+                                        newTable[key] = result[key]
+                                    }
+                                })
+                                if (Object.keys(newTable).length > 1) {
+                                    entityTable[table].push(newTable)
+                                }
+                                if (table === 'individual') {
+                                    idList[result.individual_id] = true;
+                                }
+                            }    
+                        }
+                    }
+                } else if (tableKeys === 'organization') {
+                    if (result.entity_type === 'entity' && result.entity_name) {
+                        let id = result.entity_name;
+                        let hash = crypto.createHash('sha1');
+                        result.organization_id = hash.update(id, 'utf8').digest('base64')
+                        for (const table in entityTable) {
+                            if (table === 'organization' && idList[result.organization_id]) {
+                                // I can't get this statement to work inversely for some reason
+                            } else {
+                                const newTable = {}
+                                filter[tableKeys][table].forEach((key) => {
+                                    if (result[key]) {
+                                        newTable[key] = result[key]
+                                    }
+                                })
+                                if (Object.keys(newTable).length > 1) {
+                                    entityTable[table].push(newTable)
+                                }
+                                if (table === 'individual') {
+                                    idList[result.organization_id] = true;
+                                }
+                            }    
+                        }
+                    }
+                } else if (tableKeys === 'vessel') {
+                    if (result.entity_type === 'vessel') {
+                        let id = ''
+                        if (result.vessel_registration_identification) {
+                            id = result.vessel_registration_identification
+                        } else if (result.entity_name) {
+                            id = result.entity_name;
+                        } else {
+                            id = `${Date.now()}`;
+                        }
+                        let hash = crypto.createHash('sha1');
+                        result.vessel_id = hash.update(id, 'utf8').digest('base64')
+                        for (const table in entityTable) {
+                            if (table === 'vessel' && idList[result.vessel_id]) {
+                                // I can't get this statement to work inversely for some reason
+                            } else {
+                                const newTable = {}
+                                filter[tableKeys][table].forEach((key) => {
+                                    if (result[key]) {
+                                        newTable[key] = result[key]
+                                    }
+                                })
+                                if (Object.keys(newTable).length > 1) {
+                                    entityTable[table].push(newTable)
+                                }
+                                if (table === 'individual') {
+                                    idList[result.vessel_id] = true;
+                                }
+                            }    
+                        }
+                    }
+                } else if (tableKeys === 'aircraft') {
+                    if (result.entity_type === 'aircraft') {
+                        let id = ''
+                        if (result.aircraft_tail_number) {
+                            id = result.aircraft_tail_number
+                        } else if (result.previous_aircraft_tail_number) {
+                            id = result.previous_aircraft_tail_number;
+                        }
+                        let hash = crypto.createHash('sha1');
+                        result.aircraft_id = hash.update(id, 'utf8').digest('base64')
+                        for (const table in entityTable) {
+                            if (table === 'aircraft' && idList[result.aircraft_id]) {
+                                // I can't get this statement to work inversely for some reason
+                            } else {
+                                const newTable = {}
+                                filter[tableKeys][table].forEach((key) => {
+                                    if (result[key]) {
+                                        newTable[key] = result[key]
+                                    }
+                                })
+                                if (Object.keys(newTable).length > 1) {
+                                    entityTable[table].push(newTable)
+                                }
+                                if (table === 'individual') {
+                                    idList[result.aircraft_id] = true;
+                                }
+                            }    
+                        }
+                    }
+                }
+            })
+            console.log(`Created ${tableKeys}.json successfully`);
+            fs.writeFileSync(`./json/${tableKeys}.json`, JSON.stringify(entityTable, null, "\t"), (err) => {
+                if (err) {
+                    throw err;
+                };
+            })
+        }
     })
+    .catch((err) => console.error(err));
